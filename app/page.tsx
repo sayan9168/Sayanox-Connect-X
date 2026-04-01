a85f74d60ac2f300200e381a34875cd47e3e2b8b
Sayan the researcher <sm6881164@gmail.com>
2026-04-01 19:24:49 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar";
+import ReelContainer from "@/components/ReelContainer";
+
+export default function Home() {
+  return (
+    <main className="bg-black">
+      <ReelContainer />
+      <Navbar />
+    </main>
+  );
+}
